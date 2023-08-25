4683b45921e3aa1164af6e8cdb7b53cf34b0fe9f
6408506480 <142033156+6408506480@users.noreply.github.com>
2023-08-24 20:53:46 -0700

    JSX Condition
    JSX Condition

---
+++ Condition.jsx
@@ -0,0 +1,26 @@
+//JSX Condition
+
+const App = () => {
+ const status= false;
+
+ if(status==true){
+  return (
+    <div>
+     <h1>Login Status</h1>
+     <button>Logout</button>
+    </div>
+  );
+ }
+
+ else{
+  return (
+    <div>
+     <h1>Login Status</h1>
+     <button>Login</button>
+    </div>
+  );
+ }
+
+};
+
+export default App;
